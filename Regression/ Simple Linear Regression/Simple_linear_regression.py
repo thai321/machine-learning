ffebbaa4b26bec3cd6f9f1336047f3b6496c7efa
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-07-01 01:38:26 -0700

    Linear Regressions graphs in python and notes

--- Regression/ Simple Linear Regression/Simple_linear_regression.py
+++ Regression/ Simple Linear Regression/Simple_linear_regression.py
@@ -10,12 +10,12 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('Salary_Data.csv')
 X = dataset.iloc[:, :-1].values # Take all the columns's values except the last one
-y = dataset.iloc[:, 3].values # take a last column' values, purchase column
+y = dataset.iloc[:, 1].values # take a last column' values, purchase column
 
 # ********** Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.2, random_state = 0) 
-# If there are 10 observations in the data, we will have 2 in the test set and 8 in the train set
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 1/3, random_state = 0) 
+# There are 30 observations in the data, we will have 10 in the test set and 20 in the train set
 
 
 # ********** Feature Scaling
@@ -23,3 +23,30 @@ X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.2, random
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)"""
+
+
+# Fitting Simple Linear Regression to the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression() # we create a machine regressor in linear model
+regressor.fit(X_train, y_train) # make it learn, train it base on the values of X_train and y_train vectors
+
+# Predicting the Test set results
+y_pred = regressor.predict(X_test) #Create the vector of prediction
+
+
+# Visualising the Training set results
+plt.scatter(X_train, y_train, color = 'red') # actually data
+plt.plot(X_train,regressor.predict(X_train), color = 'blue') # predicting the salary base on values of X_train vector
+plt.title('Salary vs Experience (Training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+
+# Visualising the Test set results
+plt.scatter(X_test, y_test, color = 'red') # actually data
+plt.plot(X_train,regressor.predict(X_train), color = 'blue') # predicting the salary base on values of X_train vector
+plt.title('Salary vs Experience (Test set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()

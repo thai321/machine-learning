5779e1ab4c406bf81db4d4a1b7c4370669519e9f
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-06-30 23:13:15 -0700

    adding feature scaling

--- Data Preprocessing/data_preprocessing.py
+++ Data Preprocessing/data_preprocessing.py
@@ -4,7 +4,7 @@
 
 @author: ThaiNguyen
 """
-# Importing the libararies
+# Importing the libararies 
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -16,7 +16,7 @@ dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values # Take all the columns's values except the last one
 y = dataset.iloc[:, 3].values # take a last column' values, purchase column
 
-# Missing Data processing
+# ********** Missing Data processing
 from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values="NaN", strategy="mean", axis=0) # replace the missing value with the mean values
 imputer = imputer.fit(X[:, 1:3]) # take columns 1 and 2 's values
@@ -34,10 +34,14 @@ X = onehotencoder.fit_transform(X).toarray()
 labelencoder_y = LabelEncoder()
 y = labelencoder_y.fit_transform(y)
 
-# Splitting the dataset into the Training set and Test set
+# ********** Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.2, random_state = 0) 
 # there are 10 observations in the data, we will have 2 in the test set and 8 in the train set
 
 
-
+# ********** Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
